a8172edaa52bb113e0aa629c0259dfc6f863c225
Sebastian DiLorenzo <sebastian.dilorenzo@bils.se>
2015-10-22 14:51:42 +0200

    Update description of remove_unpaired.py

--- remove_unpaired.py
+++ remove_unpaired.py
@@ -5,6 +5,18 @@ import sys
 #Author: Sebastian DiLorenzo
 #Date: 2015-22-10
 
+#What this script is for:
+#If you for some reason have a SAM or BAM file with broken mate pairs (one mate missing) that is queryname sorted
+#and the samtools flags are broken for said reads as well. Note that if the flag is not broken you can use samtools to the same effect as this script. 
+# This script will remove the single reads to one file (singles.sam) and the paired reads to another file (paired.sam).
+#For me this occurred in my bismark aligned paired end data, perhaps due to deduplicate_bismark but that is not confirmed.
+
+#Usage BAM file:
+#samtools view -h input.bam | ./remove_unpaired.py
+
+#Usage SAM file:
+#./remove_unpaired.py input.sam
+
 #Take each line from the input file (which has been piped in as sam format using samtools) and
 #remove lines that are unpaired.
 
